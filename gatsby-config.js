a6ec10023847cdd705f31cf5d202933fb9b9b814
Monknow <juanrodriguezcode@gmail.com>
2021-09-25 11:47:13 -0400

    Se ha añadido iconos y banners de compartir

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< HEAD
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "Higher or Lower YT",
@@ -25,4 +26,62 @@ module.exports = {
       __key: "images",
     },
   ],
+=======
+	siteMetadata: {
+		siteUrl: "https://peaceful-austin-24fb09.netlify.app/",
+		title: "Higher or Lower YT",
+	},
+	plugins: [
+		"gatsby-plugin-styled-components",
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/assets/images/higher-or-lower-yt-logo.png",
+				name: `Higher or Lower YouTube`,
+				short_name: `Higher Lower YT`,
+				start_url: `/`,
+				background_color: `#252422`,
+				theme_color: `#fff`,
+				display: `standalone`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-alias-imports`,
+			options: {
+				alias: {
+					"@animations": path.resolve(__dirname, "src/animations"),
+					"@assets": path.resolve(__dirname, "src/assets"),
+					"@components": path.resolve(__dirname, "src/components"),
+					"@data": path.resolve(__dirname, "src/data"),
+					"@functions": path.resolve(__dirname, "src/functions"),
+					"@global": path.resolve(__dirname, "src/global"),
+					"@hooks": path.resolve(__dirname, "src/hooks"),
+					"@pages": path.resolve(__dirname, "src/pages"),
+				},
+				extensions: [],
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/assets/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-plugin-react-svg",
+			options: {
+				rule: {
+					include: /\.inline\.svg$/,
+				},
+			},
+		},
+	],
+>>>>>>> ec221d3 (Se ha añadido iconos y banners de compartir)
 };
